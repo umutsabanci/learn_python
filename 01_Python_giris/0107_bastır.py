b90f879bdab65bda1bca054efa4fa13375fbbbb8
umutsabanci <ahleew17@gmail.com>
2021-03-08 21:57:18 +0300

    08032021

---
+++ 01_Python_giris/0107_bastır.py
@@ -0,0 +1,25 @@
+ad= "Umut"
+soyAd="Sabancı"
+
+print("Ad:  " + ad)
+
+
+
+sehir1 = "İstanbul"
+sehir2 = "Danimarka"
+
+mesafe = "2800"
+
+print(sehir1 + "  ile  " + sehir2 + "  arası  " + mesafe + "  dir ")
+
+
+a = 1
+b = 4
+c = 2
+
+delta = b**2-4*a*c
+
+print("Delta değeri: ",delta)
+
+
+

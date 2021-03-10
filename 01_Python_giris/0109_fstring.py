1dbb66da05eae4cb35fab1aaacfc3f0c4797eae1
umutsabanci <ahleew17@gmail.com>
2021-03-10 21:33:30 +0300

    10032021

---
+++ 01_Python_giris/0109_fstring.py
@@ -0,0 +1,5 @@
+rakam = float(input("Lütfen 0-9 arasında bir rakam giriniz. \t"))
+
+print("Gridiğiniz rakamın bir fazlası: " + str(rakam+1))
+
+print(f"Gridiğiniz {rakam} rakamın bir fazlası: {rakam+1}")
---
+++ 01_Python_giris/0110_uygulama.py
@@ -0,0 +1,8 @@
+sayi1 = float(input("Birinci sayıyı giriniz: "))
+sayi2 = float(input("İkinci sayıyı gitiniz: "))
+
+print(f"{sayi1} + {sayi2} = {sayi1+sayi2}")
+print(f"{sayi1} - {sayi2} = {sayi1-sayi2}")
+print(f"{sayi1} * {sayi2} = {sayi1*sayi2}")
+print(f"{sayi1} / {sayi2} = {sayi1/sayi2}")
+
